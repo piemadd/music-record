2febaf33adefabbe60a8e263fc21ad303e5f4dac
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2023-05-03 09:32:40 -0500

    added details to error page.

--- src/error.jsx
+++ src/error.jsx
@@ -6,6 +6,12 @@ const ErrorPage = () => {
     <div>
       <h1>Oops!</h1>
       <p>{error.message}</p>
+      <p>
+        Seems like we ran into an error, probably with getting data from
+        spotify. Please <a href='/'>try again</a> and{" "}
+        <a href='mailto:piero@piemadd.com'>email me</a> if you continue to face
+        problems.
+      </p>
     </div>
   );
 };
